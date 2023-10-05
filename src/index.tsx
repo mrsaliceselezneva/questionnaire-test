a08bd6e8d6f3caf0a19a6e1e7dfff141935991fe
Svetlana Tolpegina <s.tolpegina@mediasof.team>
2023-10-05 12:13:06 +0400

    form-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as React from "react";
 
 const rootElement = document.getElementById("root");
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement);
+const root = ReactDOM.createRoot(rootElement as HTMLElement)!;
 
 root.render(
     // <React.StrictMode>
-    // <BrowserRouter>
-    <App />,
-    // </BrowserRouter>,
+    <BrowserRouter>
+        <App />,
+    </BrowserRouter>,
     // </React.StrictMode>,
 );
