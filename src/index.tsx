c04f2c5f27c41f53cb1676e9c29dc61a0cf62f10
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2023-10-18 15:39:52 +0400

    create component GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import React from "react";
-import colors from "styles/colors";
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    box-sizing: border-box;
-  }
-`;
+import GlobalStyle from "styles/GlobalStyle";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
